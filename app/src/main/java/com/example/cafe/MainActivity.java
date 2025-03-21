f55470e6ab3864e8f950cbb371e663be9409eed1
Alina Andreeva <runcannyvalley@gmail.com>
2025-03-21 19:45:39 +0300

    add activity_order_detail.xml

--- app/src/main/java/com/example/cafe/MainActivity.java
+++ app/src/main/java/com/example/cafe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cafe;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -14,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, OrderDetailActivity.class);
+        startActivity(intent);
     }
 }
