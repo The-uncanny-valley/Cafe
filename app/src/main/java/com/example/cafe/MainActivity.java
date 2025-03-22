7388dcc974507f2d0d2ce14a42c0ad968a2a4d4e
Alina Andreeva <runcannyvalley@gmail.com>
2025-03-22 18:30:14 +0300

    Added OnClickListener for the sign-In button with field validation and error messages for empty inputs

--- app/src/main/java/com/example/cafe/MainActivity.java
+++ app/src/main/java/com/example/cafe/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.cafe;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText; // chatGPT
+import android.widget.Toast;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,12 +17,50 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private Button buttonSignIn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, OrderDetailActivity.class);
+        initViews();
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = editTextUsername.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+
+                if (!username.isEmpty() && !password.isEmpty()) {
+                    launchNextScreen(username, password);
+                } else {
+                    Toast.makeText(
+                            MainActivity.this,
+                            "Fill in all fields",
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+            }
+        });
+
+    }
+
+    private void launchNextScreen(String username, String password) {
+        Intent intent = new Intent(MainActivity.this, MakeOrderActivity.class);
+        intent.putExtra("username", username);
+        intent.putExtra("password", password);
         startActivity(intent);
     }
+
+    private void initViews() { // created to make code clear
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonSignIn = findViewById(R.id.buttonSignIn);
+    }
 }
+
