72adf7f7d3b4be5696f7cbb0ad5a0b2d92144687
Alina Andreeva <runcannyvalley@gmail.com>
2025-03-25 20:23:17 +0300

    add initViews

--- app/src/main/java/com/example/cafe/MakeOrderActivity.java
+++ app/src/main/java/com/example/cafe/MakeOrderActivity.java
@@ -3,6 +3,11 @@ package com.example.cafe;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +16,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MakeOrderActivity extends AppCompatActivity {
+
+    private TextView textViewGreeting;
+    private TextView textViewAdditives;
+
+    private RadioGroup radioGroupDrinks;
+    private RadioButton radioButtonTea;
+    private RadioButton radioButtonCoffee;
+
+    private CheckBox checkBoxMilk;
+    private CheckBox checkBoxSugar;
+    private CheckBox checkBoxLemon;
+
+    private Spinner spinnerTea;
+    private Spinner spinnerCoffee;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +42,28 @@ public class MakeOrderActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
     }
 
     public static Intent newIntent(Context context, String userName) { // Factory Method
         Intent intent = new Intent(context, MakeOrderActivity.class);
         intent.putExtra("username", userName);
         return intent;
+    }
+
+    private void initViews() {
+        textViewGreeting = findViewById(R.id.textViewGreeting);
+        textViewAdditives = findViewById(R.id.textViewAdditives);
+
+        radioGroupDrinks = findViewById(R.id.radioGroupDrinks);
+        radioButtonTea = findViewById(R.id.radioButtonTea);
+        radioButtonCoffee = findViewById(R.id.radioButtonCoffee);
+
+        checkBoxMilk = findViewById(R.id.checkBoxMilk);
+        checkBoxSugar = findViewById(R.id.checkBoxSugar);
+        checkBoxLemon = findViewById(R.id.checkBoxLemon);
+
+        spinnerTea = findViewById(R.id.spinnerTea);
+        spinnerCoffee = findViewById(R.id.spinnerCoffee);
     }
 }
